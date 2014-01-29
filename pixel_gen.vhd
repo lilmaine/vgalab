37753320df9d21a7d4c84e523fdac67f8311e8bb
tramaine <lilmaine@gmail.com>
2014-01-29 11:24:16 -0700

    initial commit

---
+++ pixel_gen.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    10:31:46 01/29/2014 
+-- Design Name: 
+-- Module Name:    pixel_gen - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity pixel_gen is
+    Port ( row : in  STD_LOGIC_VECTOR (10 downto 0);
+           column : in  STD_LOGIC_VECTOR (10 downto 0);
+           blank : in  STD_LOGIC;
+           r : out  STD_LOGIC_VECTOR (7 downto 0);
+           g : out  STD_LOGIC_VECTOR (7 downto 0);
+           b : out  STD_LOGIC_VECTOR (7 downto 0));
+end pixel_gen;
+
+architecture Behavioral of pixel_gen is
+
+begin
+
+
+end Behavioral;
+
