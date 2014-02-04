ddf21294cd302af26b47ff27a4d7238c6d45ebe9
tramaine <lilmaine@gmail.com>
2014-02-04 15:57:44 -0700

    v_sync working

--- vgasync.vhd
+++ vgasync.vhd
@@ -39,93 +39,97 @@ entity h_sync_gen is
 end h_sync_gen;
 
 architecture look_ahead_moore of h_sync_gen is
-	type hsync_state is
+	type state_type is
 		(active_video, front_porch, sync, back_porch, completed_state);
 	signal count_reg: unsigned(10 downto 0):= "00000000000";
-	signal state_reg, state_next: hsync_state: hsync_state;
+	signal state_reg, state_next: state_type;
 	signal hsync_reg, hsync_next, blank_reg, blank_next, completed_reg, completed_next: std_logic;
 	signal column_reg, column_next, count_next: unsigned (10 downto 0);
 
 
 begin
 	-- state register
-	count_next <= count_reg + 1;
-	column <= count;
 	process(clk, reset)
 	begin
 		if (reset='1') then
 			state_reg <= active_video;
-		elsif (clk' event and clk='1') then
+		elsif rising_edge(clk) then
 			state_reg <= state_next;
 		end if;
 	end process;
 	
 	--output buffer
-	process(clk, reset)
+	process(clk)
 	begin
-		if (reset='1') then
-			hsync_reg <= '0';
-			blank_reg <=	'1';
-			completed_reg <= '0';
-			column_reg <=  '0';
-		elsif (clk' event and clk='1') then
+		if rising_edge(clk) then
 			hsync_reg <= hsync_next;
 			blank_reg <=	blank_next;
 			completed_reg <= completed_next;
 			column_reg <=  column_next;
-		end if;	
+		end if;
+	end process;
+	
+	count_next <= 	(others => '0') when state_reg /= state_next else 
+						count_reg + 1;
+	
+	--count register
+	
+	process(clk, reset)
+	begin
+		if (reset ='1') then
+			count_reg <= (others => '0');
+		elsif rising_edge(clk) then
+			count_reg <= count_next;
+		end if;
+	end process;	
+			
+	
 	--next-state logic
 		process(state_reg, count_reg)
 		begin
+			state_next <= state_reg;
+		
 			case state_reg is
 				when active_video =>
-					if (count_reg < 640) then
-						state_next <= active_video;
-					else
+					if (count_reg >= 640) then
 						state_next <= front_porch;
 					end if;
 				when front_porch =>
-					if (count_reg < 656) then
-						state_next <= front_porch;
-					else
+					if (count_reg >= 16) then
 						state_next <= sync;
 					end if;
 				when sync =>
-					if (count_reg < 752) then
-						state_next <= sync;
-					else
+					if (count_reg >= 96) then
 						state_next <= back_porch;
 					end if;
 				when back_porch =>
-					if (count_reg < 799) then
-						state_next <= back_porch;
-					else
+					if (count_reg >= 47) then
 						state_next <= completed_state;
 					end if;				
 				when completed_state =>
-					state_next => active_video;
+					state_next <= back_porch;
 			end case;		
 	end process;
 	
 --column <= count_reg when count_reg < 640 else "00000000000"; 
 
 	--look ahead output logic
-	process(state_next)
+	process(state_next, count_reg)
 	begin
 			hsync_next <= '1';
 			blank_next <=	'1';
 			completed_next <= '0';
-			column_next <=  count_next;
+			column_next <=  (others => '0');
 			case state_next is
 				when active_video =>
 					blank_next <=	'0';
+					column_next <= count_reg;
 				when front_porch =>
 				when sync =>
 					hsync_next <= '0';
 				when back_porch =>
 				when completed_state =>
 					completed_next <= '1';
-					column_next <=  '0';
 			end case;
 	end process;
 	--output
