01ccc5d9baffc8a184be19a4ee770c01ecaa41a3
tramaine <lilmaine@gmail.com>
2014-02-04 15:00:43 -0700

    changed EVERYTHING... made it look like p. 346

--- vgasync.vhd
+++ vgasync.vhd
@@ -22,7 +22,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
@@ -35,52 +35,104 @@ entity h_sync_gen is
            h_sync : out  STD_LOGIC;
            blank : out  STD_LOGIC;
            completed : out  STD_LOGIC;
-           column : out  STD_LOGIC_VECTOR (10 downto 0));
+           column : out  unsigned (10 downto 0));
 end h_sync_gen;
 
-architecture Behavioral of h_sync_gen is
+architecture look_ahead_moore of h_sync_gen is
+	type hsync_state is
+		(active_video, front_porch, sync, back_porch, completed_state);
+	signal count_reg: unsigned(10 downto 0):= "00000000000";
+	signal state_reg, state_next: hsync_state: hsync_state;
+	signal hsync_reg, hsync_next, blank_reg, blank_next, completed_reg, completed_next: std_logic;
+	signal column_reg, column_next, count_next: unsigned (10 downto 0);
 
-signal count: unsigned(10 downto 0):= "00000000000";
-signal count_next: unsigned(10 downto 0);
 
 begin
-	count_next <= count + 1;
+	-- state register
+	count_next <= count_reg + 1;
 	column <= count;
 	process(clk, reset)
 	begin
 		if (reset='1') then
-			count <= "00000000000";
-		elsif (clk'event and clk='1') then
-			if (count < 640) then
-				count <= count_next;
-				h_sync <= '1';
-				blank <= '0';
-				completed <= '0';
-			elsif (count < 656) then
-				count <= count_next;
-				h_sync <= '1';
-				blank <= '1';
-				completed <= '0';
-			elsif (count < 752) then
-				count <= count_next;
-				h_sync <= '0';
-				blank <= '1';
-				completed <= '0';		
-			elsif (count < 799) then
-				count <= count_next;
-				h_sync <= '1';
-				blank <= '1';
-				completed <= '0';				
-			else 
-				count <= count_next;
-				h_sync <= '1';
-				blank <= '1';
-				completed <= '1';					
-			end if;	
-		end if;	
+			state_reg <= active_video;
+		elsif (clk' event and clk='1') then
+			state_reg <= state_next;
+		end if;
 	end process;
+	
+	--output buffer
+	process(clk, reset)
+	begin
+		if (reset='1') then
+			hsync_reg <= '0';
+			blank_reg <=	'1';
+			completed_reg <= '0';
+			column_reg <=  '0';
+		elsif (clk' event and clk='1') then
+			hsync_reg <= hsync_next;
+			blank_reg <=	blank_next;
+			completed_reg <= completed_next;
+			column_reg <=  column_next;
+		end if;	
+	--next-state logic
+		process(state_reg, count_reg)
+		begin
+			case state_reg is
+				when active_video =>
+					if (count_reg < 640) then
+						state_next <= active_video;
+					else
+						state_next <= front_porch;
+					end if;
+				when front_porch =>
+					if (count_reg < 656) then
+						state_next <= front_porch;
+					else
+						state_next <= sync;
+					end if;
+				when sync =>
+					if (count_reg < 752) then
+						state_next <= sync;
+					else
+						state_next <= back_porch;
+					end if;
+				when back_porch =>
+					if (count_reg < 799) then
+						state_next <= back_porch;
+					else
+						state_next <= completed_state;
+					end if;				
+				when completed_state =>
+					state_next => active_video;
+			end case;		
+	end process;
+	
+--column <= count_reg when count_reg < 640 else "00000000000"; 
 
-column <= std_logic_vector(count) when count < 640 else "00000000000"; 
+	--look ahead output logic
+	process(state_next)
+	begin
+			hsync_next <= '1';
+			blank_next <=	'1';
+			completed_next <= '0';
+			column_next <=  count_next;
+			case state_next is
+				when active_video =>
+					blank_next <=	'0';
+				when front_porch =>
+				when sync =>
+					hsync_next <= '0';
+				when back_porch =>
+				when completed_state =>
+					completed_next <= '1';
+					column_next <=  '0';
+			end case;
+	end process;
+	--output
+	h_sync <= hsync_reg;
+	blank <=	blank_reg;
+	completed <= completed_reg;
+	column <=  column_reg;
 
-end Behavioral;
+end look_ahead_moore;
 
