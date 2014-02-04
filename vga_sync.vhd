c596f7574b84c162cb59c381098b8f4ab3336662
tramaine <lilmaine@gmail.com>
2014-02-04 10:39:21 -0700

    instantiated vsync and hsync

--- vga_sync.vhd
+++ vga_sync.vhd
@@ -32,18 +32,61 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity vga_sync is
     Port ( clk : in  STD_LOGIC;
            reset : in  STD_LOGIC;
-           h_sync : in  STD_LOGIC;
-           v_sync : in  STD_LOGIC;
-           v_completed : in  STD_LOGIC;
-           blank : in  STD_LOGIC;
-           row : in  STD_LOGIC_VECTOR (10 downto 0);
-           column : in  STD_LOGIC_VECTOR (10 downto 0));
+           h_sync : out  STD_LOGIC;
+           v_sync : out  STD_LOGIC;
+           v_completed : out  STD_LOGIC;
+           blank : out  STD_LOGIC;
+           row : out  STD_LOGIC_VECTOR (10 downto 0);
+           column : out  STD_LOGIC_VECTOR (10 downto 0));
 end vga_sync;
 
 architecture Behavioral of vga_sync is
 
+	component h_sync_gen
+    Port ( clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           h_sync : out  STD_LOGIC;
+           blank : out  STD_LOGIC;
+           completed : out  STD_LOGIC;
+           column : out  STD_LOGIC_VECTOR (10 downto 0));
+	end component;
+
+	component v_sync_gen is
+    Port ( clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           h_completed : in  STD_LOGIC;
+           v_sync : out  STD_LOGIC;
+           blank : out  STD_LOGIC;
+           completed : out  STD_LOGIC;
+           row : out  STD_LOGIC_VECTOR (10 downto 0));
+	end component;
+
+--entity work.v_sync_gen(Behavioral);
+
+signal hsync_sig, hblank_sig, hcompleted_sig, vblank_sig: std_logic_vector;
+
 begin
 
+	Inst_h_sync_gen: entity work.h_sync_gen(Behavioral) PORT MAP(
+		clk => clk,
+		reset => reset,
+		h_sync => h_sync,
+		blank => hblank_sig,
+		completed => hcompleted_sig,
+		column => column
+	);
+
+	Inst_v_sync_gen:  entity work.v_sync_gen(Behavioral) PORT MAP(
+		clk => clk,
+		reset => reset,
+		h_completed => hcompleted_sig,
+		v_sync => vsync,
+		blank => vblank_sig,
+		completed => v_completed,
+		row => row
+	);
+	
+blank <= vblank_sig or hblank_sig;
 
 end Behavioral;
 
