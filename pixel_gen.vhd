597c4184649a6a90584d61b72c5fa87a62986d2e
tramaine <lilmaine@gmail.com>
2014-02-06 10:37:40 -0700

    yea

--- pixel_gen.vhd
+++ pixel_gen.vhd
@@ -46,8 +46,6 @@ begin
 process(row, column, blank, switch_1, switch_2)
 begin
 	if(blank = '0') then
---case 1
-	if (switch_1 = '0' and switch_2 = '0') then
 		if(row < 400) then
 			if(column < 210) then
 				r <= (others => '0');
@@ -67,74 +65,6 @@ begin
 		g <= (others => '0');
 		b <= "11111111";			
 		end if;
-	end if;
---case 2
-	if (switch_1 = '0' and switch_2 = '0') then
-		if(row < 400) then
-			if(column < 210) then
-				r <= "11111111";
-				g <= "11111000";
-				b <= (others => '0');
-			elsif(column >= 210 and column < 420) then
-				r <= (others => '0');
-				g <= "00001111";
-				b <= "11111111";
-			else
-				r <= "00001111";
-				g <= "00000111";
-				b <= "11111100";
-			end if;
-	else
-		r <= "00000111";
-		g <= "00000000";
-		b <= "11111111";			
-		end if;
-	end if;
---case 3
-	if (switch_1 = '0' and switch_2 = '0') then
-		if(row < 400) then
-			if(column < 210) then
-				r <= "10000000";
-				g <= "00000001";
-				b <= "00000111";
-			elsif(column >= 210 and column < 420) then
-				r <= (others => '0');
-				g <= (others => '0');
-				b <= "11111111";
-			else
-				r <= "11111111";
-				g <= (others => '0');
-				b <= (others => '0');
-			end if;
-	else
-		r <= "11111111";
-		g <= (others => '0');
-		b <= "11111111";			
-		end if;
-	end if;
---case 4
-	if (switch_1 = '0' and switch_2 = '0') then
-		if(row < 400) then
-			if(column < 210) then
-				r <= (others => '0');
-				g <= "11111111";
-				b <= (others => '0');
-			elsif(column >= 210 and column < 420) then
-				r <= (others => '0');
-				g <= (others => '0');
-				b <= "11111111";
-			else
-				r <= "11111111";
-				g <= (others => '0');
-				b <= (others => '0');
-			end if;
-	else
-		r <= "00000011";
-		g <= (others => '0');
-		b <= "00000000";			
-		end if;
-	end if;	
-	
 	end if;
 end process;
 
